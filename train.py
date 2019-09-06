22edcad5c9373a82f82f42ad74758463a0728984
Mukesh Yadav <mak.gnu@gmail.com>
2019-09-06 13:43:40 +0530

    Added proposal file

--- train.py
+++ train.py
@@ -1,23 +1,33 @@
 import os
 import sys
 import warnings
+import pandas as pd
+from environments import TradingEnvironment
+from exchanges.simulated import SimulatedExchange
+from actions import DiscreteActionStrategy
+from rewards import SimpleProfitStrategy
+import matplotlib.pyplot as plt
+
 
 def warn(*args, **kwargs):
     pass
 
 warnings.warn = warn
 warnings.simplefilter(action='ignore', category=FutureWarning)
-
-
 sys.path.append(os.path.dirname(os.path.abspath('')))
 
-from environments import TradingEnvironment
-from exchanges.simulated import FBMExchange
-from actions import DiscreteActionStrategy
-from rewards import SimpleProfitStrategy
+def get_date(symbol):
+    df = pd.read_parquet(f"/data/stocks/{symbol}_1min.parquet.gzip")
+    df['date'] = pd.to_datetime(df['date']).astype(int)/ 10**9
+    return df
 
-exchange = FBMExchange()
-action_strategy = DiscreteActionStrategy()
+instrument_symbol = "ABB"
+
+df = get_date(instrument_symbol)
+
+df.index += 1
+exchange = SimulatedExchange(data_frame=df)
+action_strategy = DiscreteActionStrategy(instrument_symbol=instrument_symbol)
 reward_strategy = SimpleProfitStrategy()
 
 env = TradingEnvironment(exchange=exchange,
@@ -28,13 +38,13 @@ obs = env.reset()
 sell_price = 1e9
 stop_price = -1
 
-print('Initial portfolio: ', exchange.portfolio)
-
-for i in range(1000):
+print('Initial portfolio: ', exchange.portfolio, len(df))
+results = []
+for i in range(len(df)-1):
     action = 0 if obs['close'] < sell_price else 18
     action = 19 if obs['close'] < stop_price else action
     
-    if i == 0 or portfolio['BTC'] == 0:
+    if i == 0 or portfolio[instrument_symbol] == 0:
         action = 16
         sell_price = obs['close'] + (obs['close'] / 50)
         stop_price = obs['close'] - (obs['close'] / 50)
@@ -44,8 +54,20 @@ for i in range(1000):
     filled_trade = info['filled_trade']
     portfolio = exchange.portfolio
     
-    print('Obs: ', obs)
-    print('Reward: ', reward)
-    print('Portfolio: ', portfolio)
-    print('Trade executed: ', executed_trade.trade_type, executed_trade.price, executed_trade.amount)
-    print('Trade filled: ', filled_trade.trade_type, filled_trade.price, filled_trade.amount)
+    results.append({
+        'reward': reward,
+        'trade_type':executed_trade.trade_type,
+        'executed_trade.price': executed_trade.price,
+        'executed_trade.amount':executed_trade.amount,
+        'filled_trade.trade_type': filled_trade.trade_type,
+        'filled_trade.price': filled_trade.price,
+        'filled_trade.amount':filled_trade.amount,
+        'portfolio':portfolio
+        
+    })
+
+result = pd.DataFrame(results)
+# # result['executed_trade.amount'].plot()
+# # result['reward'].plot()
+# # plt.show()
+# print(result)
